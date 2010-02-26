3b620eabb42b02572c8cf239d841452890ecfe83
Martijn Meijers <b.m.meijers@tudelft.nl>
2010-02-26 13:46:56 +0100

    New version

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
-from setuptools import setup
-import sys,os
-from setuptools.extension import Extension
-libs = []
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "fake_pyrex"))
+from Cython.Distutils import build_ext
+from setuptools import setup, Extension
+
 setup(
     name = "predicates",
     version = "alpha",
@@ -11,14 +14,7 @@ setup(
     description = "",
     url = "https://ssl.zw9.nl/svn/code/",
     package_dir = {'':'src'},
-    ext_modules = [
-    Extension("predicates",
-#        ["src/predicates.pyx"],
-        sources = ['src/predicates.c', 'src/shewchuk.c'],
-        libraries = libs,
-        extra_compile_args = [ "-O2", "-Wall", "-funroll-loops" ],
-        extra_link_args = [ "-O2" ],
-    ),
-    ],
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension("predicates", 
+                             sources = ["src/predicates.pyx", "src/shewchuk.c"])],
 )
-
