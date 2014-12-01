acb77cac5dfeac6636668fdc7c146d25a3f820b7
Martijn Meijers <b.m.meijers@tudelft.nl>
2014-12-01 15:33:39 +0100

    Adding missing headers.

--- make.sh
+++ make.sh
@@ -10,6 +10,6 @@ rm src/predicates/shewchuk.o
 gcc -O0 -o bounds src/predicates/shewchuk_init.c
 ./bounds > src/predicates/shewchuk_init.h
 cython src/predicates/_predicates.pyx
-gcc -O0 -fPIC -Isrc/predicates -I/usr/include/python2.7 -c src/predicates/_predicates.c -o src/predicates/_predicates.o
-gcc -O0 -fPIC -Isrc/predicates -I/usr/include/python2.7 -c src/predicates/shewchuk.c -o src/predicates/shewchuk.o
-gcc -shared src/predicates/_predicates.o src/predicates/shewchuk.o -L/usr/lib -lpython2.7 -o src/predicates/_predicates.so
+gcc -O0 -fPIC -Isrc/predicates -I/usr/include/python2.6 -c src/predicates/_predicates.c -o src/predicates/_predicates.o
+gcc -O0 -fPIC -Isrc/predicates -I/usr/include/python2.6 -c src/predicates/shewchuk.c -o src/predicates/shewchuk.o
+gcc -shared src/predicates/_predicates.o src/predicates/shewchuk.o -L/usr/lib -lpython2.6 -o src/predicates/_predicates.so
