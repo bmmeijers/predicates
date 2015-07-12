b8ce464219dde3f77073e0fa5df159e3b12a0524
Martijn Meijers <b.m.meijers@tudelft.nl>
2015-07-12 15:07:57 +0200

    Back to development: 1.0.0

--- src/geompreds/__init__.py
+++ src/geompreds/__init__.py
@@ -1,7 +1,7 @@
 """A Python wrapper for 2D predicates of Jonathan Richard Shewchuk
 """
 
-__version__ = '0.1.2'
+__version__ = '1.0.0.dev0'
 __license__ = 'MIT License'
 __author__ = 'Martijn Meijers'
 __creation_date__ = '2010-02-26'
